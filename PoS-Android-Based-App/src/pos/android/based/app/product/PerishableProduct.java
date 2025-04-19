f534905e520d121b44faf482b02f7e09005261f0
rielyta <desi@students.usu.ac.id>
2025-04-19 16:27:14 +0700

    bundle

--- PoS-Android-Based-App/src/pos/android/based/app/product/PerishableProduct.java
+++ PoS-Android-Based-App/src/pos/android/based/app/product/PerishableProduct.java
@@ -8,6 +8,22 @@ package pos.android.based.app.product;
  *
  * @author Desi
  */
-public class PerishableProduct {
-    
+import java.time.LocalDate;
+
+public class PerishableProduct extends Product {
+    private LocalDate expiryDate;
+
+    public PerishableProduct(String id, String name, int stock, double price, LocalDate expiryDate) {
+        super(id, name, stock, price, "perishable"); // ✅ pastikan pakai constructor yang benar
+        this.expiryDate = expiryDate;
+    }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " | Exp: " + expiryDate;
+    }
 }
