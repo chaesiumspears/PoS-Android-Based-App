a0f81170f1f2165612f2cf72b8dfe44f2d668fd1
rielyta <desi@students.usu.ac.id>
2025-04-22 13:40:59 +0700

    komentar

--- PoS-Android-Based-App/src/pos/android/based/app/product/BundleProduct.java
+++ PoS-Android-Based-App/src/pos/android/based/app/product/BundleProduct.java
@@ -11,17 +11,19 @@ package pos.android.based.app.product;
 import java.util.List;
 
 public class BundleProduct extends Product {
+    // List Bundle terdiri dari daftar objek Product
     private List<Product> items;
 
+    //Konstruktor untuk BundleProduct,Memanggil konstruktor superclass dengan tipe bundle, dan menginisialisasi daftar item produk.
     public BundleProduct(String id, String name, double price, int stock, List<Product> items) {
         super(id, name,stock, price, "bundle");
         this.items = items;
     }
-
+    //getter
     public List<Product> getItems() {
         return items;
     }
-
+    //total diskon: harga semua produk - harga bundle 
     public double getDiscountedPrice() {
         double total = 0;
         for (Product p : items) total += p.price;
